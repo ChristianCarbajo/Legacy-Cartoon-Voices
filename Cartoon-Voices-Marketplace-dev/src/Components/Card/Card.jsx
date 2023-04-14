5f6f80d5b843bab95b57d7809b12de907426b7cd
Ashur19 <iglesiasjota1911@gmail.com>
2023-04-14 11:18:48 +0200

    Like Button 1st

--- Cartoon-Voices-Marketplace-dev/src/Components/Card/Card.jsx
+++ Cartoon-Voices-Marketplace-dev/src/Components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Card.css'
+import HeartButton from "../HeartButton/HeartButton";
 
 
 function Card(props) {
@@ -14,12 +15,16 @@ function Card(props) {
             <div className="card-img ">
                 <img src={props.img} alt={props.name} width="200vh" height="200vh" />
             </div>
-            <ul class="card-body-ul">
-                <h5 className="card-title">{props.name}</h5>
-                <li className="list-group-item">Categoria: {props.category}</li>
-                <li className="list-group-item">{props.price}€</li>
-                <li className="list-group-item">email: {props.email}</li>
-            </ul>
+            <div className="card-body-content">
+                <ul className="card-body-ul">
+                    <h5 className="card-title">{props.name}</h5>
+                    <li className="list-group-item">Categoria: {props.category}</li>
+                    <li className="list-group-item">{props.price}€</li>
+                    <li className="list-group-item">email: {props.email}</li>
+                </ul>
+                <HeartButton/>
+            </div>
+            
             <div className="card-body-button">
                 <Link to="/contact">
                     <button type="button" className="btn btn-secondary btn-sm m-1">Contratar</button>
--- Cartoon-Voices-Marketplace-dev/src/Components/Seeker/Seeker.jsx
+++ Cartoon-Voices-Marketplace-dev/src/Components/Seeker/Seeker.jsx
@@ -13,16 +13,16 @@ function Seeker(props) {
       .catch((error) => console.error(error));
   }, []);
 
-  useEffect(() => {
-    const results = data.filter((item) => {
-      const titleMatch = item.title.toLowerCase().includes(searchTerm.toLowerCase());
-      const priceMatch = item.price.toLowerCase().includes(searchTerm.toLowerCase());
-      const categoriesMatch = item.category.toLowerCase().includes(searchTerm.toLowerCase());
-      const mailMatch = item.email.toLowerCase().includes(searchTerm.toLowerCase());
-      return titleMatch || priceMatch || categoriesMatch || mailMatch;
-    });
-    setFilteredData(results);
-  }, [searchTerm, data]);
+  // useEffect(() => {
+  //   const results = data.filter((item) => {
+  //     const titleMatch = item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  //     const priceMatch = item.price.toLowerCase().includes(searchTerm.toLowerCase());
+  //     const categoriesMatch = item.category.toLowerCase().includes(searchTerm.toLowerCase());
+  //     const mailMatch = item.email.toLowerCase().includes(searchTerm.toLowerCase());
+  //     return titleMatch || priceMatch || categoriesMatch || mailMatch;
+  //   });
+  //   setFilteredData(results);
+  // }, [searchTerm, data]);
 
   const itemsToRender = filteredData.length > 0 ? filteredData : data;
 
