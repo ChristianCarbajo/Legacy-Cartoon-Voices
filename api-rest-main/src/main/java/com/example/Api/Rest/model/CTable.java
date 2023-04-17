daf5e2197cfce51e6cccfc2b0edbcbaba3cbdda9
ChristianCarbajo <christian_carbajo@hotmail.com>
2023-04-17 10:02:09 +0200

    relacional table

---
+++ api-rest-main/src/main/java/com/example/Api/Rest/model/CTable.java
@@ -0,0 +1,20 @@
+package com.example.Api.Rest.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name="voices_comments")
+public class CTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="voice_id")
+    private CVoice cVoice;
+
+    @Column(name="comment")
+    private  String comment;
+
+}
